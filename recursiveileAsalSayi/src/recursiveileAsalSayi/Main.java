b2daa03a3a06d21bd83229b2c43c3f07d76bc714
caglaakin <114911261+caglaakin@users.noreply.github.com>
2023-02-07 16:21:23 +0300

    Add files via upload

---
+++ recursiveileAsalSayi/src/recursiveileAsalSayi/Main.java
@@ -0,0 +1,22 @@
+package recursiveileAsalSayi;
+
+import java.util.Scanner;
+
+public class Main {
+	static boolean asal(int n, int i) {
+		if(i == 1) return true;
+		if(n <= 1) return false;
+		if(n % i == 0) return false;
+		return asal(n, i-1);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Sayı giriniz : ");
+		int number = scan.nextInt();
+		int i = number-1;
+		System.out.println(asal(number, i)?"Asal sayıdır.":"Asal sayı değildir.");
+
+	}
+
+}
